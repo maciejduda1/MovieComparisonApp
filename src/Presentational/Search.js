30b6f5cd4af4fed76a96c170391e61513471f0cb
Maciej Duda <maciej.duda1@gazeta.pl>
2018-05-07 11:15:06 +0200

    search bar button

--- src/Presentational/Search.js
+++ src/Presentational/Search.js
@@ -2,18 +2,25 @@ import React from 'react';
 
 const Search = (props) => (
     <div className="search-box">
-        <input 
-            type='text'
-            onKeyUp = {(event) => {
-                if (event.keyCode === 13 && event.target.value.length > 4) {
-                    return props.searchDatabase(event.target.value);
-                } 
-            }}     
-            placeholder = 'Searched Movie Title (min. 5 signs) + Enter'
-        /> 
+        <form onSubmit={e => e.preventDefault()}>
+            <input 
+                type='text'
+                
+                ref={(input) => this.input = input}     
+                placeholder = 'Movie Title'
+            /> 
+            <input type='submit' value='Submit' onClick={(event) => props.searchDatabase(this.input.value)}/>
+        </form>
     </div>
 );
 
+/*
+                onKeyUp = {(event) => {
+                    if (event.keyCode === 13) {
+                        return props.searchDatabase(this.input.value);
+                    } 
+                }}
+                */
 export default Search;
 
 
